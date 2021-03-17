79de6528a28c8978a0f64a9d17603449d5b30abd
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-17 19:15:52 -0300

    Criação dos primeiros arquivos de classes.

---
+++ correcaoSolo/src/main/java/Domain/CalcPotassio.java
@@ -0,0 +1,13 @@
+package Domain;
+
+public class CalcPotassio {
+
+    private double noSolo;
+
+    public double getNoSolo(double noSolo){
+        return noSolo;
+    }
+    public void setNoSolo(double noSolo){
+        this.noSolo = noSolo;
+    }
+}
