19f8d6d7ba96561af41e2e07ce34e6188d6ec177
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-03-18 14:45:43 -0300

    Update CalcPotassio.java

--- correcaoSolo/src/main/java/Domain/CalcPotassio.java
+++ correcaoSolo/src/main/java/Domain/CalcPotassio.java
@@ -3,6 +3,7 @@ package Domain;
 public class CalcPotassio {
 
     private double noSolo;
+    private int textSolo;
 
     public double getNoSolo(double noSolo){
         return noSolo;
@@ -10,4 +11,7 @@ public class CalcPotassio {
     public void setNoSolo(double noSolo){
         this.noSolo = noSolo;
     }
+
+    public double getTextSolo(int textSolo) { return textSolo; };
+    public void setTextSolo(int textSolo) {this.textSolo = textSolo; };
 }
